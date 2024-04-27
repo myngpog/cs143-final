9ba9741cbccfd2f1456ca6cdaa3a4c856936e1db
My <pingpengpongpog@gmail.com>
2024-04-26 22:43:46 -0700

    help me please

---
+++ services/postgres/schema.sql
@@ -0,0 +1,73 @@
+CREATE EXTENSION postgis; --extension that helps with location-based data
+
+\set ON_ERROR_STOP on --ensures no subsequent commands are run if an error occurs
+
+BEGIN;
+
+CREATE EXTENSION postgis;
+
+\set ON_ERROR_STOP on
+
+BEGIN;
+
+/*
+ * Users may be partially hydrated with only a name/screen_name 
+ * if they are first encountered during a quote/reply/mention 
+ * inside of a tweet someone else's tweet.
+ */
+CREATE TABLE users (
+    id_users BIGINT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    urls TEXT,
+    friends_count INTEGER,
+    listed_count INTEGER,
+    favourites_count INTEGER,
+    statuses_count INTEGER,
+    protected BOOLEAN,
+    verified BOOLEAN,
+    screen_name TEXT,
+    name TEXT,
+    location TEXT,
+    description TEXT,
+    withheld_in_countries VARCHAR(2)[]
+);
+
+/*
+ * Tweets may be entered in hydrated or unhydrated form.
+ */
+CREATE TABLE tweets (
+    id_tweets BIGINT,
+    id_users BIGINT,
+    created_at TIMESTAMPTZ,
+    in_reply_to_status_id BIGINT,
+    in_reply_to_user_id BIGINT,
+    quoted_status_id BIGINT,
+    retweet_count SMALLINT,
+    favorite_count SMALLINT,
+    quote_count SMALLINT,
+    withheld_copyright BOOLEAN,
+    withheld_in_countries VARCHAR(2)[],
+    source TEXT,
+    text TEXT,
+    country_code VARCHAR(2),
+    state_code VARCHAR(2),
+    lang TEXT,
+    place_name TEXT,
+    geo geometry
+);
+
+CREATE TABLE tweet_urls (
+    id_tweets BIGINT,
+    urls TEXT
+);
+
+/*
+ * =====INDEXES=====
+ */
+create index on tweet_tags(tag, id_tweets);
+create index on tweets(id_tweets, lang);
+create index on tweet_tags(id_tweets, tag);
+create index on tweets using gin(to_tsvector('english',text)) where lang = 'en';
+
+COMMIT;
