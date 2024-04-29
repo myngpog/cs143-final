b6c56ece5dbd158ae2cd0c11715343e5ee3db4ea
My <pingpengpongpog@gmail.com>
2024-04-29 09:49:11 -0700

    test check

--- services/postgres/schema.sql
+++ services/postgres/schema.sql
@@ -4,19 +4,13 @@ CREATE EXTENSION postgis; --extension that helps with location-based data
 
 BEGIN;
 
-CREATE EXTENSION postgis;
-
-\set ON_ERROR_STOP on
-
-BEGIN;
-
 /*
  * Users may be partially hydrated with only a name/screen_name 
  * if they are first encountered during a quote/reply/mention 
  * inside of a tweet someone else's tweet.
  */
 CREATE TABLE users (
-    id_users BIGINT,
+    id_users BIGINT primary key,
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
     urls TEXT,
@@ -30,24 +24,24 @@ CREATE TABLE users (
     name TEXT,
     location TEXT,
     description TEXT,
-    withheld_in_countries VARCHAR(2)[]
+    -- withheld_in_countries VARCHAR(2)[]
 );
 
 /*
  * Tweets may be entered in hydrated or unhydrated form.
  */
 CREATE TABLE tweets (
-    id_tweets BIGINT,
-    id_users BIGINT,
+    id_tweets BIGINT primary key,
+    id_users BIGINT primary key,
     created_at TIMESTAMPTZ,
-    in_reply_to_status_id BIGINT,
-    in_reply_to_user_id BIGINT,
-    quoted_status_id BIGINT,
+    -- in_reply_to_status_id BIGINT,
+    -- in_reply_to_user_id BIGINT,
+    -- quoted_status_id BIGINT,
     retweet_count SMALLINT,
     favorite_count SMALLINT,
     quote_count SMALLINT,
-    withheld_copyright BOOLEAN,
-    withheld_in_countries VARCHAR(2)[],
+    -- withheld_copyright BOOLEAN,
+    -- withheld_in_countries VARCHAR(2)[],
     source TEXT,
     text TEXT,
     country_code VARCHAR(2),
@@ -58,16 +52,17 @@ CREATE TABLE tweets (
 );
 
 CREATE TABLE tweet_urls (
-    id_tweets BIGINT,
+    id_tweets BIGINT primary key,
     urls TEXT
 );
 
 /*
  * =====INDEXES=====
  */
-create index on tweet_tags(tag, id_tweets);
-create index on tweets(id_tweets, lang);
-create index on tweet_tags(id_tweets, tag);
-create index on tweets using gin(to_tsvector('english',text)) where lang = 'en';
+create index idx_users_sc on users(screen_name); --look for usernames
+create index idx_created_at on users(created_at); --look for users based on acc creation
+create index idx_location on users(location); --filter on location quickly
+
+
 
 COMMIT;
